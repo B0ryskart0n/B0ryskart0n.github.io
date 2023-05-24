d53a6415cc98a07ffc6b107d971320a4d9dcb8b2
B0ryskart0n <boryskopec00@gmail.com>
2023-05-24 23:33:32 +0200

    Update file structure

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,6 @@ input.addEventListener("keypress", e => {
 function verifyPassword() {
     const input = document.getElementById("password").value;
     if (input == "hasło") {
-        document.location.replace("content.html");
+        document.location.replace("pages/content.html");
     }
 }
